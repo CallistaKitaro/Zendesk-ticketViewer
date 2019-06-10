5d596b0a048bfec3aed67dfe81df1df5333f104a
Callista Kitaro <callista.kitaro@gmail.com>
2019-06-10 13:16:00 +1000

    Initialize basic vue and express structure

---
+++ build/routes/api/getTickets.js
@@ -0,0 +1,25 @@
+// const fetch = require('node-fetch');
+const axios = require('axios');
+
+module.exports = (app) => {
+    app.get( '/getTickets', (req, res) => {
+
+		const baseUrl = 'https://callista.zendesk.com/api/v2/tickets.json';	
+
+		axios.get(baseUrl, {
+            headers: { 
+                'Authorization': 'Basic ',
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => res.data) 
+		.then(data => {
+			res.send({ data });
+		})
+		.catch(err => {
+            console.log('err: ', err);
+		});
+
+	})
+}
---
+++ src/components/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    hello here
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA',
+      // info: {
+      //   "ticket": {
+      //     "subject":  "My printer is on fire!",
+      //     "comment":  { "body": "The smoke is very colorful." },
+      //     "priority": "urgent"
+      //   }
+      // }
+    }
+  },
+  methods: {
+    fetch () {
+      axios.get('/getTickets').then(response => {
+        console.log(response)
+      })
+      // axios.get('/api/v2/tickets.json').then(response => {
+      //   console.log(response)
+      // })
+    }
+  },
+  created () {
+    this.fetch()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
